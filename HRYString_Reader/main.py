ec4425cedd403ed4ff2eee411026d16750510279
cathalba <107694612+cathalba@users.noreply.github.com>
2023-05-18 05:38:19 -0700

    Add files via upload
    Updating main.py file

--- HRYString_Reader/main.py
+++ HRYString_Reader/main.py
@@ -6,7 +6,6 @@
 import re
 
 
-
 def find_zero_indices(string):
     indices = []
     for i, char in enumerate(string):
@@ -20,16 +19,16 @@ def search_string_in_file(file_path, search_string, regex, fail_count):
     try:
         with open(file_path, 'r') as file:
             found = False
-            #print("searching for" + '"HRYIndex": ' + search_string)
+            # print("searching for" + '"HRYIndex": ' + search_string)
             for line in file:
                 if str('"HRYIndex": ' + search_string) in line:
-                    ##print(f"Found '{search_string}' in line {line_number}:")
-                    #print(line)
+                    # print(f"Found '{search_string}' in line {line_number}:")
+                    # print(line)
                     found = True
                 if found and re.search(regex, line) and (fail_count == 0):
                     fail = re.search(regex, line).group(regex_group)
                     fail_count += 1
-                    #print("HRY partition to ignore = " + str(fail))
+                    # print("HRY partition to ignore = " + str(fail))
                     failing_par = str(fail)
                     partition_list.append(failing_par)
             if not found:
@@ -37,15 +36,10 @@ def search_string_in_file(file_path, search_string, regex, fail_count):
     except FileNotFoundError:
         print("File not found.")
 
+# binary_string = '11111100000111001111111'
 
 
-
-# Example usage
-
-
-#binary_string = '11111100000111001111111'
-binary_string = input("please provide your HRY string: ")
-
+binary_string = input("Please provide your binary HRY string: ")
 zero_indices = find_zero_indices(binary_string)
 print("HRY indices:", zero_indices)
 regex = r"HRYPrint\"\: \"(.*)(\")"
@@ -55,14 +49,14 @@ partition_list = []
 partition_str = str("PartitionsToIgnore = \"")
 count = 0
 
-#file_path = "C:\\Users\\cathalba\\source\\repos\\lnl\\Modules\\SCN_CCF\\InputFiles\\UCLK_HRY.json"
-file_path = input("please provide your HRY json  filepath: ")
+# file_path = "C:\\Users\\cathalba\\source\\repos\\lnl\\Modules\\SCN_CCF\\InputFiles\\UCLK_HRY.json"
+file_path = input("{lease provide your HRY .json filepath: ")
 
 for element in zero_indices:
     element = str(element)
     search_string_in_file(file_path, element, regex, fail_count)
-    #partition_list.append(failing_par)
-    #print("failng par = " + failing_par)
+    # partition_list.append(failing_par)
+    # print("Failing par = " + failing_par)
 
 
 #print(partition_list)
